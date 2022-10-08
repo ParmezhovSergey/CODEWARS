1d1f873833339ed92b8dbc380794424905158b75
ParmezhovSergey <parmezhovsergey@mail.ru>
2022-10-08 20:43:49 +0300

    9

---
+++ 47.js
@@ -0,0 +1,12 @@
+// Месси – футболист, забивший голы в трех лигах:
+
+// Ла Лига
+// Копа дель Рей
+// Чемпионы
+// Завершите функцию, чтобы вернуть его общее количество голов во всех трех лигах.
+
+// Примечание: ввод всегда будет действительным.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
